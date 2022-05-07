d292071226a32b9985529a07b028d0b3df740e07
Vladislav Karnaukhov <karnaukhov.vl@gmail.com>
2022-05-07 17:33:47 +0700

    -

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import { createContext, React } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
+import { Client } from '../src/services/Client';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ export const ThemeContext = createContext(null);
+
 root.render(
-  <React.StrictMode>
+    <ThemeContext.Provider 
+  value={{
+    resp: new Client()
+  }}>
     <App />
-  </React.StrictMode>
+    </ThemeContext.Provider>
 );
 
